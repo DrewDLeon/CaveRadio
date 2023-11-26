27523aae12d72ab110a1a98bc39393b80fcef90c
Vladimir Guzman <vladimirbarcenas84@gmail.com>
2023-11-26 01:37:08 -0600

    Database configuration

---
+++ Migrations/20231126065113_songs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaveRadio.Migrations
+{
+    /// <inheritdoc />
+    public partial class songs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- obj/Debug/net8.0/CaveRadio.AssemblyInfo.cs
+++ obj/Debug/net8.0/CaveRadio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaveRadio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca2a23859050e4ad847f5cf11abbbf25abc09d8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08777e2d4986f460d7c859f0c70884f56f6737e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaveRadio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaveRadio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
