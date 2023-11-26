989641444dd7ca1e07932b0c27962b86d5649354
Andres De Leon Castilleja <andleon.castilleja@gmail.com>
2023-11-26 02:41:09 -0600

    changes again

---
+++ Models/SongViewModelList.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaveRadio.Models
+{
+    public class SongViewModelList
+    {
+        public List<SongViewModel> songs { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using CaveRadio;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDBContext> (
+    opc => opc.UseNpgsql("name=MiConeccionBD")
+);
 
 var app = builder.Build();
 
